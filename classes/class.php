e51a0e95b1828e3e8940ded8cffca2540fcd5678
LeleGuidi <emanuele.guidi98@outlook.it>
2022-06-27 15:41:13 +0200

    Inserita function construct e funzione pubblica

--- classes/class.php
+++ classes/class.php
@@ -7,6 +7,20 @@ class Film {
     public $oscar;
     public $price;
 
+    function __construct($_title, $_director, $_oscar, $_price)
+    {
+        $this->title = $_title;
+        $this->director = $_director;
+        $this->oscar = $_oscar;
+        $this->price = $_price;
+    }
 
+    public function getDiscount() {
+        if ($this->oscar) {
+            return $this->price * 0.8;
+        }
+        return false;
+    }
+};
 
 ?>
